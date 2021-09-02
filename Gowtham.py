0b84d8d1b85d6b2771aff246caac02cd77cfe304
Gowtham Dakshnamoorthy <gowthamdakshnamoorthy@gmail.com>
2021-09-01 18:29:31 -0700

    Commiting Homework

--- Gowtham.py
+++ Gowtham.py
@@ -1,7 +1,6 @@
 from scipy.optimize import minimize
-
 funcn = lambda x: (x[0] - x[1]) ** 2 + (x[1] + x[2] - 2) ** 2 + (x[3] - 1) ** 2 + (x[4] - 1) ** 2
-const = ({'type': 'eq', 'fun': lambda x: x[0] + 3 * x[1]},
+const = ({'type': 'eq', 'func': lambda x: x[0] + 3 * x[1]},
         {'type': 'eq', 'func': lambda x: x[2] + x[3] - 2 * x[4]},
         {'type': 'eq', 'func': lambda x: x[1] - x[4]})
 bounds = ((-10, 10), (-10, 10), (-10, 10), (-10, 10), (-10, 10))
